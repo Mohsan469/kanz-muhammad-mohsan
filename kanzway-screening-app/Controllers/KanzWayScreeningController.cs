5bda65caec8216759f4486a04506c84f04c0b9a2
mhiqbal <mhiqbal@abana.com.sa>
2024-12-07 07:12:49 +0300

    Created screening Test REST API

---
+++ kanzway-screening-app/Controllers/KanzWayScreeningController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kanzway_screening_app.Controllers
+{
+    [ApiController]
+    public class KanzWayScreeningController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/v1/screening/{number}")]
+        public List<string> GetStrings(UInt32 number)
+        {
+            if (number == 0)
+            {
+                return null;
+            }
+            var list = new List<string>();
+            try
+            {
+                var numbers = Enumerable.Range(1, (int)number).ToList();
+                foreach (var item in numbers)
+                {
+                    list.Add(item.ToString());
+
+                    if (item % 3 == 0 && item % 5 == 0)
+                    {
+                        list.Add("KanzWay");
+                    }
+                    else if (item % 3 == 0)
+                    {
+                        list.Add("Kanz");
+                    }
+                    else if (item % 5 == 0)
+                    {
+                        list.Add("Way");
+                    }
+                }
+                return list;
+            }
+            catch (Exception ex)
+            {
+                logger.log.Error("ERROR",ex);
+            }
+            return list;
+        }
+    }
+}
